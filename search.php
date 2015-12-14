eb9dab533cf69672d362c0a01ade57f5d5b9cad6
thridenx <srgmfrr@gmail.com>
2015-12-14 18:22:32 +0000

    variável passa
    variável passa
    corrigir query de adicionar para não ter membros da equipa
    fazer página de erro
    fazer css em falta
    fazer admin quem cria a equipa
    meeting active?????

--- search.php
+++ search.php
@@ -2,41 +2,33 @@
     require('ref.php');
     require('init.php');
     require('dadosEquipa.php');
-
     $output='';
-
     if (isset($_POST['searchVal'])){
         $searchq=$_POST['searchVal'];
         //echo $searchq;
       	$searchq=preg_replace("#[^0-9a-z]#i", "", $searchq);
-
      //  $query=mysqli_query("SELECT username, email, name FROM user WHERE username like '%'".$searchq."'%' or name like '%''".$searchq."'%' or email like '%''".$searchq."'%'");
         //$query=mysqli_query($db_select,"SELECT username, email, name FROM user WHERE username like '%a%' or name like '%a%' or email like '%a%'");
-        $query=mysqli_query($db_select,"SELECT username FROM user WHERE username like '%$searchq%'");
-
-
+        $query=mysqli_query($db_select,"SELECT user.username, user.user_id FROM user WHERE username like '%$searchq%'");
         $count=mysqli_num_rows($query);
         //var_dump($query);
-
         if ($count == 0){
-            $output= "Não existe nenhum valor!";
+            $output= "Não encontrei utilizadores!";
         }
-        else{
+        else if ($searchq=''){
+            $output='';
+        }else{
+            $output .= '<form class="" action="addteam.php" method="POST">';
+            $i = 0;
             while ($row=mysqli_fetch_array($query)) {
                 $fuser=$row['username'];
-               // $femail=$row['email'];
-                //$fname=$row['name']; 
-                
-                $output ='<div>'.$fuser.'</div>';
-                //echo $output;
-                //echo $output;
+                $fuser_id=$row['user_id'];
+                $output .='<p><input class="indigo accent-4 btn" type="submit" name="username" value="'.$fuser.'"></p>';
+                $output .= '<input type="hidden" name="user_id" value="'.$fuser_id.'">';
+                $i++;
             }
-
+            $output.='</form>';
         }
-
     }
-
-
-
     echo $output;
     ?>
