2f0ae5decb2814b96f127720a01bc69c5fdfae60
jessicaparente <jessica.aparente@gmail.com>
2015-12-14 19:36:26 +0000

    coco da erro na team create

--- team-create.php
+++ team-create.php
@@ -16,61 +16,58 @@
         echo "<script language='javascript' type='text/javascript'>alert('Nome de equipa já existente!');window.location.href='registo.php';</script>";	
     }else{
         
-        
-        
         //cria um chat
         $insert_chat = "INSERT INTO chat(name) VALUES ('".$teamname."_chat')";
         $result_inserir_chat = mysqli_query($db_select, $insert_chat);
         
         if($result_inserir_chat){
-        
+            
             if (mysqli_query($db_select, $insert_chat)) {
                 $last_id = mysqli_insert_id($db_select);
             }
-            /*
-            //buscar nome 
-            $select_chat_id ="SELECT chat_id FROM chat WHERE name='".$teamname."_chat' ";
-            $check = mysqli_query($db_select,$select_chat_id);
-            $array3= array();
-                while($check_number =mysqli_fetch_array($check)){
-                    array_push($array3, $check_number); 
-             }
-    
-            if(sizeof($check_number)>=1){*/
-            if ($last_id!=''){
-                $array= array();
-                while($count =mysqli_fetch_array($check)){
-                    array_push($array, $count); 
-                }
-                $chat_id =$array[0][0];
-                
-                $insert_team = "INSERT into team(name, chat_id, workfield, skills)
-                VALUES ('".$teamname."', '".$last_id."', '".$workfield."' , '".$skills."')";
-                if (mysqli_query($db_select, $insert_team)) {
-                    $team_id = mysqli_insert_id($db_select);
-                }
-                $result_insert_team = mysqli_query($db_select,$insert_team);
-                if($result_insert_team){
-                    $add_relationship ="
-                    INSERT into team_user( team_id, user_id)
-                    VALUES('".$team_id."', '". $user_id."') ";
-                     $result_add_relationship = mysqli_query($db_select,$add_relationship);
-                    if($result_add_relationship){
-                        mysqli_query($db_select, "COMMIT");
-                        echo '<meta http-equiv="refresh" content="0; URL=team-view.php">';
-                    }else{
-                        mysqli_query($db_select, "ROLLBACK");
-                        echo 'erro na ultima merda #pobres :(';
+
+            if(sizeof($check_number)>=1){
+                if ($last_id!=''){
+                    $array= array();
+                    while($count =mysqli_fetch_array($check)){
+                         array_push($array, $count); 
                     }
+                    $chat_id =$array[0][0];
+                echo "cenas";
+                    $insert_team = "INSERT into team(name, chat_id, workfield, skills)
+                    VALUES ('".$teamname."', '".$last_id."', '".$workfield."' , '".$skills."')";
+                    if (mysqli_query($db_select, $insert_team)) {
+                        $team_id = mysqli_insert_id($db_select);
+                    }
+                    echo "cenas2";
+                    $result_insert_team = mysqli_query($db_select,$insert_team);
+                    
+                    if($result_insert_team){
+                        $add_relationship ="INSERT into team_user( team_id, user_id)
+                        VALUES('".$team_id."', '".$user_id."') ";
+                        $result_add_relationship = mysqli_query($db_select,$add_relationship);
+                        echo "cenas3";
+                        if($result_add_relationship){
+                            echo "cenas4";
+                            mysqli_query($db_select, "COMMIT");
+                            echo '<meta http-equiv="refresh" content="0; URL=team-view.php">';
+                        
+                        }else{
+                            mysqli_query($db_select, "ROLLBACK");
+                            echo 'erro na ultima merda #pobres :(';
+                        }
+                    }
+                    else{
+                        mysqli_query($db_select, "ROLLBACK");
+                        echo 'erro a criar team';
+                    }
+                
                 }else{
                     mysqli_query($db_select, "ROLLBACK");
-                    echo 'erro a criar team';
+                    echo 'erro a buscar id do chat';
                 }
-            }else{
-                mysqli_query($db_select, "ROLLBACK");
-                echo 'erro a buscar id do chat';
             }
-        }else{
+        else{
             mysqli_query($db_select, "ROLLBACK");
             echo 'erro a inserir chat, erro';
         }
