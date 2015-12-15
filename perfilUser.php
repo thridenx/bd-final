cbd79cd0960916b5d36bfb08e05d96d4e0fef77b
jessicaparente <jessica.aparente@gmail.com>
2015-12-15 22:27:51 +0000

    quase feito o perfil

--- perfilUser.php
+++ perfilUser.php
@@ -7,33 +7,35 @@
         require('ref.php');
         include 'sessao.php';
         require('dados.php');
+        include 'header.php';
     ?>
 
     <body>
-        <div id="infUser">
-            <div id="nome">
-                <? echo "$nome"?>
+        <div id="infUser" class="container">
+            <div id="nome"><br><br>
+                <h3><? echo "$nome"?></h3>
             </div>
             <!--TITULO-->
-            <div id="username">
-                <? echo "$username"?>
+            <div id="username"><h5>
+                <? echo "$username"?></h5><br>
             </div>
+            <ul class="collection">
             <div id="infGeral">
-                <div id="sex">sexo:
+                <li class="collection-item"><div id="sex"><h6>sexo:</h6>
                     <? echo " $sex"?>
-                </div>
-                <div id="birth">data de nascimento:
+                </div></li>
+                <li class="collection-item"><div id="birth"><h6>data de nascimento:</h6>
                     <? echo " $birth"?>
-                </div>
-                <div id="email">e-mail:
+                </div></li>
+                <li class="collection-item"><div id="email"><h6>e-mail:</h6>
                     <? echo " $email"?>
-                </div>
-                <div id="website">website:
+                </div></li>
+                <li class="collection-item"><div id="website"><h6>website:</h6>
                     <? echo " $website"?>
-                </div>
-                <div id="equipas">equipas a que pertence:
+                </div></li>
+                <li class="collection-item"><div id="equipas"><h6>equipas a que pertence:</h6>
                     <? echo " $equipas"?>
-                </div>
+                </div></li>
             </div>
         </div>
         </div>
